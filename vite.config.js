36479fb4d60e4176a858cefc9e4536943eda9590
syedtalha88 <150226683+syedtalha88@users.noreply.github.com>
2024-07-20 19:58:20 +0530

    config change

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/T.mart-Food/",
+  // base:"/T.mart-Food/",
   plugins: [react()],
 })
